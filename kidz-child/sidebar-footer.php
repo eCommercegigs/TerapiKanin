242da605a69275b3e5d6ceb525152e1438f1a0f0
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2018-11-07 12:23:55 +0100

    Customized contacts info in footer

--- kidz-child/sidebar-footer.php
+++ kidz-child/sidebar-footer.php
@@ -1,9 +1,7 @@
 <div class="row footer-sidebar">
-	<div class="col-md-3 col-sm-6 col-xs-6 first">
+	<div class="col-md-3 col-sm-6 col-xs-6 first widget">
 		<?php if ( ideapark_mod( 'footer_contacts' ) ) { ?>
-			<div class="contacts">
 				<?php echo make_clickable( str_replace( ']]>', ']]&gt;', ideapark_mod( 'footer_contacts' ) ) ); ?>
-			</div>
 		<?php } ?>
 	</div>
 	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( ideapark_mod( 'footer_sidebar' ) ? ideapark_mod( 'footer_sidebar' ) : 'footer-sidebar' ) ) {} ?>
